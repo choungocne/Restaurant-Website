afa8828c7a223b6bbea11f352c79fc10bd9d979e
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-24 23:30:11 +0700

    add CustomerRepository.cs and ICustomerRepository.cs

---
+++ RestaurantWebsite/Repository/CustomerRepository.cs
@@ -0,0 +1,41 @@
+﻿using RestaurantWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantWebsite.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Customer> GetAll() => _context.Customers.ToList();
+
+        public Customer GetById(int id) => _context.Customers.Find(id);
+
+        public void Add(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
+
+        public void Update(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var customer = GetById(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
