75d88adc7c56f1b94987dd99ff6cdf3fac20b4cb
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-24 23:11:58 +0700

    add Employee

--- RestaurantWebsite/Models/Employee.cs
+++ RestaurantWebsite/Models/Employee.cs
@@ -1,16 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
 
-namespace RestaurantWebsite.Models
+namespace RestaurantWebsite.Models;
+
+public partial class Employee
 {
-    public class Employee
-    {
-        public int EmployeeID { get; set; }
-        [Required, StringLength(255)]
-        public string FullName { get; set; }
-        [Required]
-        public DateTime BirthDate { get; set; }
-        [Required, StringLength(15)]
-        public string PhoneNummber { get; set; }
-    }
+    public int EmployeeId { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public DateOnly BirthDate { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Img { get; set; }
+
+    public string? Detail { get; set; }
+
+    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+    public virtual ICollection<UserAccount> UserAccounts { get; set; } = new List<UserAccount>();
 }
