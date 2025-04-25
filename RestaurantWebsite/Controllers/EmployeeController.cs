b90095d83fd29163d51c4e4b7757822af98efaa0
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-25 14:56:30 +0700

    fix emloyee

--- RestaurantWebsite/Controllers/EmployeeController.cs
+++ RestaurantWebsite/Controllers/EmployeeController.cs
@@ -1,86 +1,90 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Rendering;
-//using RestaurantWebsite.Models;
-//using RestaurantWebsite.Repository;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RestaurantWebsite.Models;
+using RestaurantWebsite.Repository;
 
-//namespace RestaurantWebsite.Controllers
-//{
-//    public class EmployeeController : Controller
-//    {
-//        private readonly IEmployeeRepository employeeRepository;
-//        public EmployeeController(IEmployeeRepository employeeRepository)
-//        {
-//            this.employeeRepository = employeeRepository;
-//        }
-//        public IActionResult Add()
-//        {
-//            var employees = employeeRepository.GetAll();
-//            ViewBag.Employees = new SelectList(employees, "EmployeeID","FullName");
-//            return View();
-//        }
-//        [HttpPost]
-//        public IActionResult Add(Employee employee)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                employeeRepository.Add(employee);
-//                return RedirectToAction("Index");
-//            }
-//            return View();
-//        }
-//        public IActionResult Index()
-//        {
-//            var employees = employeeRepository.GetAll();
-//            return View(employees);
-//        }
-//        public IActionResult Display(int id)
-//        {
-//            var employees = employeeRepository.GetById(id);
-//            if (employees == null)
-//            {
-//                return NotFound();
-//            }
-//            return View(employees);
-//        }
-//        public IActionResult Update(int id)
-//        {
-//            var employees = employeeRepository.GetById(id);
-//            if (employees == null)
-//            {
-//                return NotFound();
-//            }
-//            return View(employees);
-//        }
-//        [HttpPost]
-//        public IActionResult Update(Employee employee)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                employeeRepository.Update(employee);
-//                return RedirectToAction("Index");
-//            }
-//            return View();
-//        }
-//        public IActionResult Delete(int id)
-//        {
-//            var employees = employeeRepository.GetById(id);
-//            if (employees == null)
-//            {
-//                return NotFound();
-//            }
-//            return View(employees);
-//        }
-//        [HttpPost]
-//        [ActionName("Delete")]
-//        [ValidateAntiForgeryToken]
-//        public IActionResult DeleteConfirmed(int id)
+namespace RestaurantWebsite.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
 
-//        {
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
 
-//            employeeRepository.Delete(id);
-//                return RedirectToAction("Index");
-           
-//        }
-//    }
-    
-//}
+        public IActionResult Index()
+        {
+            var employees = _employeeRepository.GetAll();
+            return View(employees);
+        }
+
+        public IActionResult Add()
+        {
+            var employees = _employeeRepository.GetAll();
+            ViewBag.Employees = new SelectList(employees, "EmployeeId", "FullName");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeRepository.Add(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        public IActionResult Display(int id)
+        {
+            var employee = _employeeRepository.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var employee = _employeeRepository.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeRepository.Update(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var employee = _employeeRepository.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _employeeRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- RestaurantWebsite/Program.cs
+++ RestaurantWebsite/Program.cs
@@ -1,25 +1,22 @@
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using RestaurantWebsite.Models;
 using RestaurantWebsite.Repository;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-// dang ki dich vu
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-
-
-
+// Đăng ký các repository
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IDishRepository, DishRepository>();
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 
 var app = builder.Build();
 
@@ -27,18 +24,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.UseHttpsRedirection();
+
 app.Run();
