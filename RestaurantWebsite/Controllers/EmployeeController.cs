ae2a21b483cb65f5e29d066fcef2c398e273b29c
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-25 15:48:55 +0700

    fix EmployeeController

--- RestaurantWebsite/Controllers/EmployeeController.cs
+++ RestaurantWebsite/Controllers/EmployeeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 using RestaurantWebsite.Models;
 using RestaurantWebsite.Repository;
 
@@ -8,10 +10,12 @@ namespace RestaurantWebsite.Controllers
     public class EmployeeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public EmployeeController(IEmployeeRepository employeeRepository)
+        public EmployeeController(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
         {
             _employeeRepository = employeeRepository;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -19,25 +23,6 @@ namespace RestaurantWebsite.Controllers
             var employees = _employeeRepository.GetAll();
             return View(employees);
         }
-
-        public IActionResult Add()
-        {
-            var employees = _employeeRepository.GetAll();
-            ViewBag.Employees = new SelectList(employees, "EmployeeId", "FullName");
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Add(Employee employee)
-        {
-            if (ModelState.IsValid)
-            {
-                _employeeRepository.Add(employee);
-                return RedirectToAction("Index");
-            }
-            return View(employee);
-        }
-
         public IActionResult Display(int id)
         {
             var employee = _employeeRepository.GetById(id);
@@ -47,6 +32,46 @@ namespace RestaurantWebsite.Controllers
             }
             return View(employee);
         }
+        public IActionResult Add()
+        {
+            var employees = _employeeRepository.GetAll();
+            ViewBag.Employees = new SelectList(employees, "EmployeeId", "FullName");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Employee employee, IFormFile ImageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                // Xử lý tệp tải lên
+                if (ImageFile != null && ImageFile.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + ImageFile.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    // Đảm bảo thư mục tồn tại
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        ImageFile.CopyTo(fileStream);
+                    }
+
+                    employee.Img = uniqueFileName;
+                }
+
+                _employeeRepository.Add(employee);
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+       
 
         public IActionResult Update(int id)
         {
@@ -59,10 +84,42 @@ namespace RestaurantWebsite.Controllers
         }
 
         [HttpPost]
-        public IActionResult Update(Employee employee)
+        public IActionResult Update(Employee employee, IFormFile ImageFile)
         {
             if (ModelState.IsValid)
             {
+                // Xử lý tệp tải lên
+                if (ImageFile != null && ImageFile.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + ImageFile.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    // Đảm bảo thư mục tồn tại
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        ImageFile.CopyTo(fileStream);
+                    }
+
+                    // Xóa tệp hình ảnh cũ (nếu có)
+                    var existingEmployee = _employeeRepository.GetById(employee.EmployeeId);
+                    if (existingEmployee != null && !string.IsNullOrEmpty(existingEmployee.Img))
+                    {
+                        string oldFilePath = Path.Combine(uploadsFolder, existingEmployee.Img);
+                        if (System.IO.File.Exists(oldFilePath))
+                        {
+                            System.IO.File.Delete(oldFilePath);
+                        }
+                    }
+
+                    employee.Img = uniqueFileName;
+                }
+
                 _employeeRepository.Update(employee);
                 return RedirectToAction("Index");
             }
@@ -83,6 +140,17 @@ namespace RestaurantWebsite.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed(int id)
         {
+            var employee = _employeeRepository.GetById(id);
+            if (employee != null && !string.IsNullOrEmpty(employee.Img))
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                string filePath = Path.Combine(uploadsFolder, employee.Img);
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+            }
+
             _employeeRepository.Delete(id);
             return RedirectToAction("Index");
         }
