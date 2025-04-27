fb6ec982c1a664a106697da38b6e12edda9e89df
Chau Ngoc <ngocctb0252@ut.edu,vn>
2025-04-27 23:19:47 +0700

    add DishCategoryRepository.cs

---
+++ RestaurantWebsite/Repositories/DishCategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.Repositories
+{
+    public class DishCategoryRepository : IDishCategoryRepository
+    {
+        private readonly RestaurantContext _context;
+        public DishCategoryRepository(RestaurantContext context)
+        {
+            _context = context;
+        }
+        public DishCategory Add(DishCategory category)
+        {
+            _context.DishCategories.Add(category);
+            _context.SaveChanges();
+            return category;
+        }
+
+        public DishCategory Delete(DishCategory category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DishCategory Get(string catagoryId)
+        {
+            return _context.DishCategories.Find(catagoryId);
+        }
+
+        public DishCategory Get(DishCategory category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DishCategory> GetAll()
+        {
+            return _context.DishCategories;
+        }
+
+        public DishCategory Update(DishCategory category)
+        {
+           _context.Update(category);
+            _context.SaveChanges();
+            return category;
+        }
+    }
+}
