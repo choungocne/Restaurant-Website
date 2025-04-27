1b21a48ea3850d5409a841fe1fd70b50620f895d
Chau Ngoc <ngocctb0252@ut.edu,vn>
2025-04-27 23:21:05 +0700

    add IEmployeeRepository.cs

---
+++ RestaurantWebsite/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.Repository
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        void Add(Employee employee);
+        void Update(Employee employee);
+        void Delete(int id);
+    }
+}
