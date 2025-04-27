ce81215368f59f4315b1bcba6d03d7030c51839a
Chau Ngoc <ngocctb0252@ut.edu,vn>
2025-04-27 23:20:13 +0700

    add IDishCategoryRepository.cs

---
+++ RestaurantWebsite/Repositories/IDishCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.Repositories
+{
+    public interface IDishCategoryRepository
+    {
+        DishCategory Add(DishCategory category);
+        DishCategory Update(DishCategory category);
+        DishCategory Delete(DishCategory category);
+        DishCategory Get(DishCategory category);
+        IEnumerable<DishCategory> GetAll();
+    }
+}
