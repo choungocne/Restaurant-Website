718d2a64493603f831a991ca8ed2f96c175aa381
Chau Ngoc <ngocctb0252@ut.edu,vn>
2025-04-27 23:19:03 +0700

    add DishCategoryRepository.cs

---
+++ RestaurantWebsite/Repositories/IDishRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.Repositories
+{
+    public interface IDishRepository
+    {
+        Task<IEnumerable<Dish>> GetAllAsync();
+        Task<Dish> GetByIdAsync(int id);
+        Task AddAsync(Dish dish);
+        Task UpdateAsync(Dish dish);
+        Task DeleteAsync(int id);
+    }
+}
