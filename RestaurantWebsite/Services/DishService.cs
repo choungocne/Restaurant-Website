37d9dac3d5ea3a4ed491ac30ff3fdb05cc47e335
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-27 23:21:33 +0700

    add DishService.cs and IDishService.cs

---
+++ RestaurantWebsite/Services/DishService.cs
@@ -0,0 +1,42 @@
+﻿using RestaurantWebsite.Models;
+using RestaurantWebsite.Repositories;
+using System.Collections.Generic;
+
+namespace RestaurantWebsite.Services
+{
+    public class DishService : IDishService
+    {
+        private readonly IDishRepository _dishRepository;
+
+        public DishService(IDishRepository dishRepository)
+        {
+            _dishRepository = dishRepository;
+        }
+
+        public async Task<IEnumerable<Dish>> GetAllAsync()
+        {
+            return await _dishRepository.GetAllAsync();
+        }
+
+        public async Task<Dish> GetByIdAsync(int id)
+        {
+            return await _dishRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddAsync(Dish dish)
+        {
+            dish.CreatedAt = DateTime.UtcNow;
+            await _dishRepository.AddAsync(dish);
+        }
+
+        public async Task UpdateAsync(Dish dish)
+        {
+            await _dishRepository.UpdateAsync(dish);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _dishRepository.DeleteAsync(id);
+        }
+    }
+}
---
+++ RestaurantWebsite/Services/IDishService.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.Services
+{
+
+    public interface IDishService
+    {
+        Task<IEnumerable<Dish>> GetAllAsync();
+        Task<Dish> GetByIdAsync(int id);
+        Task AddAsync(Dish dish);
+        Task UpdateAsync(Dish dish);
+        Task DeleteAsync(int id);
+    }
+}
+
