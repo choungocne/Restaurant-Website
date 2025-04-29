60555bdea87ee3ae73299c2219f50e4ec53e7a06
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-29 20:38:26 +0700

    add EmployeeDishViewModel.cs

---
+++ RestaurantWebsite/Models/EmployeeDishViewModel.cs
@@ -0,0 +1,10 @@
+﻿using X.PagedList;
+
+namespace RestaurantWebsite.Models
+{
+    public class EmployeeDishViewModel
+    {
+        public List<Employee> Employees { get; set; }
+        public List<Dish> Dishes { get; set; }
+    }
+}
