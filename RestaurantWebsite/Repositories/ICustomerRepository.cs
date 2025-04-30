b41ac22cec1d9fd9ff5ced1ec9976f9c8574d108
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-30 23:19:31 +0700

    add ICustomerRepository.cs

---
+++ RestaurantWebsite/Repositories/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.Repository
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetAll();
+        Customer GetById(int id);
+        void Add(Customer customer);
+        void Update(Customer customer);
+        void Delete(int id);
+    }
+}
