417e862378a1546e49173fb0a8e3d0fd961cf97f
Chau Ngoc <chaungoc7909@gmail.com>
2025-04-30 23:20:26 +0700

    them html Customer

---
+++ RestaurantWebsite/ViewModels/EmployeeDishViewModel.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantWebsite.Models;
+using X.PagedList;
+
+namespace RestaurantWebsite.ViewModels
+{
+    public class EmployeeDishViewModel
+    {
+        public List<Employee> Employees { get; set; }
+        public List<Dish> Dishes { get; set; }
+        public IEnumerable<DiningTable> DiningTables { get; set; } = new List<DiningTable>();
+        public IEnumerable<OrderService> OrderServices { get; set; } = new List<OrderService>();
+    }
+}
