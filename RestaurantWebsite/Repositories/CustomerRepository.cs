cc18a0e1dac78f562f504ce89d474aef1b6e0157
Chau Ngoc <chaungoc7909@gmail.com>
2025-05-01 23:54:34 +0700

    add CustomerRepository.cs and DishCategoryViewComponent.cs

---
+++ RestaurantWebsite/Repositories/CustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.Repositories
+{
+    public class CustomerRepository
+    {
+        private static List<Customer> Customers = new List<Customer>();
+        public static IEnumerable<Customer> AllCustomers
+        {
+            get { return Customers; }
+        }
+    }
+}
---
+++ RestaurantWebsite/ViewComponents/DishCategoryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantWebsite.Repositories;
+namespace RestaurantWebsite.ViewComponents
+{
+    public class DishCategoryViewComponent : ViewComponent
+    {
+        private readonly DishCategoryRepository _dishCategory;
+        public DishCategoryViewComponent(IDishCategoryRepository dishCategoryRepository)
+        {
+            dishCategoryRepository = _dishCategory;
+        }
+        public IViewComponentResult Innoke()
+        {
+            var dishCategory = _dishCategory.GetAll().OrderBy(x=>x.CategoryName);
+            return View(dishCategory);
+        }
+    }
+}
