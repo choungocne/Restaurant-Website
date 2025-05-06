17b3ee3d555c71e4c18399c40a4b95bd91e09e4a
Chau Ngoc <chaungoc7909@gmail.com>
2025-05-06 22:19:03 +0700

    add VM

---
+++ RestaurantWebsite/ViewModels/CustomerReservationsListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantWebsite.ViewModels
+{
+    public class CustomerReservationsListViewModel
+    {
+        public List<CustomerReservationViewModel> Reservations { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
---
+++ RestaurantWebsite/ViewModels/OrderDishViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantWebsite.ViewModels
+{
+    public class OrderDishViewModel
+    {
+        public string DishName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal SubTotal { get; set; }
+        public string Note { get; set; }
+    }
+}
