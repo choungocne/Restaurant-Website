a519759a85b5fdb89764fa0075385c6e64ad6b7d
Chau Ngoc <chaungoc7909@gmail.com>
2025-05-06 22:18:42 +0700

    add CustomerReservationViewModel.cs

---
+++ RestaurantWebsite/ViewModels/CustomerReservationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.ViewModels
+{
+    public class CustomerReservationViewModel
+    {
+        public TableReservation Reservation { get; set; }
+        public OrderService OrderService { get; set; }
+        public List<OrderDishViewModel> OrderDishes { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string StatusDisplay { get; set; }
+    }
+}
