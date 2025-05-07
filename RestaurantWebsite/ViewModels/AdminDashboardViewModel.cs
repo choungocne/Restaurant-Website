dff777fcf499f3e8d49b03468aeaf492224f8cea
Chau Ngoc <chaungoc7909@gmail.com>
2025-05-08 00:14:04 +0700

    create AdminDashboardViewModel.cs and AdminRegisterViewModel.cs

---
+++ RestaurantWebsite/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantWebsite.Models;
+
+namespace RestaurantWebsite.ViewModels
+{
+    public class AdminDashboardViewModel
+    {
+        public int CustomerCount { get; set; }
+        public int DishCount { get; set; }
+        public int ReservationCount { get; set; }
+        public int EmployeeCount { get; set; }
+        public List<TableReservation> RecentReservations { get; set; } = new List<TableReservation>();
+
+    }
+}
---
+++ RestaurantWebsite/ViewModels/AdminRegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantWebsite.ViewModels
+{
+    public class AdminRegisterViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tên đăng nhập")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Tên đăng nhập phải từ 4-50 ký tự")]
+        public string Username { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Mật khẩu phải từ 6-100 ký tự")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng xác nhận mật khẩu")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không trùng khớp")]
+        public string ConfirmPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng nhập họ tên")]
+        [StringLength(255, ErrorMessage = "Họ tên không được vượt quá 255 ký tự")]
+        public string FullName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng chọn ngày sinh")]
+        [DataType(DataType.Date)]
+        public DateOnly BirthDate { get; set; }
+    }
+}
